ffed1a5f58c4fd9a6280e9d47f57a6146923464b
sbk-ofis <blackbirdstar38@gmail.com>
2016-11-25 17:55:50 +0300

    Initial commit

---
+++ app/src/main/java/com/blackbirdstar/memursen/EntryAdapter.java
@@ -0,0 +1,63 @@
+package com.blackbirdstar.memursen;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class EntryAdapter extends BaseAdapter {
+
+    private List<Response.EntriesBean> entries;
+    private Context mContext;
+    private LayoutInflater inflater;
+
+    public EntryAdapter(Context mContext, List<Response.EntriesBean> entries) {
+        this.entries = entries;
+        this.mContext = mContext;
+    }
+
+
+    @Override
+    public int getCount() {
+        return entries.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return entries.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.each_list_item, parent, false);
+
+        Response.EntriesBean item = (Response.EntriesBean) getItem(position);
+
+        ImageView thumbnail = (ImageView) rowView.findViewById(R.id.list_image);
+        TextView title = (TextView) rowView.findViewById(R.id.title);
+        TextView body = (TextView) rowView.findViewById(R.id.body);
+
+        String imageUrl = MainActivity.url + item.getLink();
+        Picasso.with(mContext).load(imageUrl).into(thumbnail);
+        //Picasso.with(mContext).load(imageUrl).networkPolicy(NetworkPolicy.OFFLINE).into(thumbnail);
+
+        title.setText(item.getTitle());
+        body.setText(item.getBody());
+
+        return rowView;
+    }
+}
